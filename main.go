f53e5834766a03170eb8b828030b6d245d19d0d7
mingi3442 <mingi3442@gmail.com>
2022-01-27 00:12:03 +0900

    10926 answer

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var hour, min int
-	fmt.Scan(&hour, &min)
-	if min >= 45 {
-		fmt.Printf("%d %d\n", hour, min-45)
-	} else {
-		if hour == 0 {
-			fmt.Printf("%d %d\n", 23, min+15)
-		} else {
-			fmt.Printf("%d %d\n", hour-1, min+15)
-		}
-	}
+	var id string
+	fmt.Scanf("%s", &id)
+	fmt.Printf("%s??!\n", id)
 }
