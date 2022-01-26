3455bd26e607a656806808b0e34a710665aaa0ef
LeeMingi <imingi@imingiui-MacBook-Pro-2.local>
2022-01-26 19:04:44 +0900

    14681 correct answer

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var year int
-	fmt.Scanf("%d", &year)
+	var x, y int
+	fmt.Scanf("%d", &x)
+	fmt.Scanf("%d", &y)
 	// fmt.Println(year)
-	if year%400 == 0 {
+	if x > 0 && y > 0 {
 		fmt.Println("1")
-	} else if year%4 == 0 && year%100 != 0 {
-		fmt.Println("1")
-	} else {
-		fmt.Println("0")
+	} else if x < 0 && y > 0 {
+		fmt.Println("2")
+	} else if x < 0 && y < 0 {
+		fmt.Println("3")
+	} else if x > 0 && y < 0 {
+		fmt.Println("4")
 	}
 }
