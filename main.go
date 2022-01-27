9199192efb10fde901050c856fa02d8721ed8287
mingi3442 <mingi3442@gmail.com>
2022-01-27 19:46:50 +0900

    8393 answer

--- main.go
+++ main.go
@@ -6,11 +6,10 @@ import (
 
 func main() {
 	var total int
+	var result int = 0
 	fmt.Scanf("%d", &total)
 	for i := 1; total+1 > i; i++ {
-		var numA, numB int
-		fmt.Scanln(&numA, &numB)
-		fmt.Println(numA + numB)
+		result += i
 	}
-
+	fmt.Println(result)
 }
