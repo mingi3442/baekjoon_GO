ad5e62f02d3c300f75aa9f48516093c8256f3099
mingi3442 <mingi3442@gmail.com>
2022-01-28 00:09:23 +0900

    15552 answer

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	var total int
-	var result int = 0
-	fmt.Scanf("%d", &total)
-	for i := 1; total+1 > i; i++ {
-		result += i
+	var a, b int
+	//reader를 통해 읽기 인스턴스 생성
+	reader := bufio.NewReader(os.Stdin)
+	//write를 통해 쓰기 인스턴스 생성
+	writer := bufio.NewWriter(os.Stdout)
+	fmt.Fscanln(reader, &total)
+
+	for i := 0; total > i; i++ {
+		fmt.Fscanln(reader, &a, &b)
+		fmt.Fprintln(writer, a+b)
 	}
-	fmt.Println(result)
+	//데이터 출력
+	writer.Flush()
 }
