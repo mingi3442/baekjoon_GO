353342605bd445203a1c21905cab02fd9d938214
mingi3442 <mingi3442@gmail.com>
2022-01-29 00:15:21 +0900

    11022 answer

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	fmt.Fscanln(reader, &total)
 	for i := 1; total+1 > i; i++ {
 		fmt.Fscanln(reader, &numA, &numB)
-		fmt.Fprintf(writer, "Case #%d: %d\n", i, numA+numB)
+		fmt.Fprintf(writer, "Case #%d: %d + %d = %d\n", i, numA, numB, numA+numB)
 	}
 	writer.Flush()
 }
