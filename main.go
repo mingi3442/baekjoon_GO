241bb29b3f53611d04b62d7d4dab8359930307ea
mingi3442 <mingi3442@gmail.com>
2022-01-31 00:14:01 +0900

    1110 answer

--- main.go
+++ main.go
@@ -7,17 +7,30 @@ import (
 )
 
 func main() {
-	var numA, numB int
+	var number, temp int
+	cycle := 0
 	reader := bufio.NewReader(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
+	fmt.Fscanln(reader, &number)
+	//newN에 입력받은 number 값 넣기
+	var newN = number
+
 	defer writer.Flush()
 	for true {
-		_, err := fmt.Fscanln(reader, &numA, &numB)
-		if err != nil {
+		//반복될 때 마다 cycle +1
+		cycle++
+		if newN < 10 {
+			//newN이 10보다 작을 경우
+			temp = newN
+		} else {
+			//10보다 클 경우
+			temp = newN/10 + newN%10
+		}
+		//다시 newN에 입력
+		newN = newN%10*10 + temp%10
+		if newN == number {
 			break
 		}
-		fmt.Fprintln(writer, numA+numB)
 	}
-
-	fmt.Fprint(writer, "\n")
+	fmt.Fprintln(writer, cycle)
 }
