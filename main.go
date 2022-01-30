b31723a261d434c864188bc8eefd1a4993dd6ce4
mingi3442 <mingi3442@gmail.com>
2022-01-30 21:40:52 +0900

    10952 answer

--- main.go
+++ main.go
@@ -7,22 +7,17 @@ import (
 )
 
 func main() {
-	var total, flag int
+	var numA, numB int
 	reader := bufio.NewReader(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
-	//첫 번째 입력 받기
-	fmt.Fscanln(reader, &total, &flag)
-	//함수 종료 직전 실행
 	defer writer.Flush()
-	// 두 번째 입력을 받기 위해 slice 선언
-	var numbers = make([]int, total)
-	//slice의 총 길이, total만큼 반복
-	for i := range numbers {
-		// 두 번째 입력을 slice에 담아서 입력 받기
-		fmt.Fscanf(reader, "%d ", &numbers[i])
-		if numbers[i] < flag {
-			fmt.Fprintf(writer, "%d ", numbers[i])
+	for true {
+		fmt.Fscanln(reader, &numA, &numB)
+		if numA == 0 && numB == 0 {
+			break
 		}
+		fmt.Fprintln(writer, numA+numB)
 	}
+
 	fmt.Fprint(writer, "\n")
 }
