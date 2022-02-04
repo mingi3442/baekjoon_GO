bc7a0d9442d9f15f395ed3801f5bfb590730ae9f
mingi3442 <mingi3442@gmail.com>
2022-02-04 22:54:30 +0900

    8958 ansswer

--- main.go
+++ main.go
@@ -7,24 +7,27 @@ import (
 )
 
 func main() {
-
+	var total int
 	reader := bufio.NewReader(os.Stdin)
+	fmt.Fscanf(reader, "%d\n", &total)
+	var arr = make([]string, total) // 입력을 받을 배열 선언
+	var temp = make([]int, total)   // 점수를 넣을 배열 선언
+	for i := 0; total > i; i++ {
+		fmt.Fscanln(reader, &arr[i])
+		var correct, score int // O의 유무와 점수를 위한 변수 선언
 
-	var numbers = make([]int, 10)
-	var rest []int
-	for i := range numbers {
-		fmt.Fscanf(reader, "%d\n", &numbers[i])
-		var temp = numbers[i] % 42
-		rest = validate(rest, temp)
-	}
-	fmt.Println(len(rest))
-}
-
-func validate(temps []int, temp int) []int {
-	for _, n := range temps {
-		if temp == n {
-			return temps
+		for j := 0; len(arr[i]) > j; j++ {
+			if fmt.Sprintf("%c", arr[i][j]) == "O" {
+				correct++
+				score += correct
+			} else {
+				correct = 0
+			}
 		}
+		temp[i] = score
 	}
-	return append(temps, temp)
+	fmt.Println(temp)
+	for _, e := range temp {
+		fmt.Println(e)
+	}
 }
