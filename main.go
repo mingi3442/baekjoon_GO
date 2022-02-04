6d263d362ddef720d494234061b1555868a9d7dc
mingi3442 <mingi3442@gmail.com>
2022-02-05 01:18:04 +0900

    11654

--- main.go
+++ main.go
@@ -7,27 +7,9 @@ import (
 )
 
 func main() {
-	var total int
+	var input byte
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Fscanf(reader, "%d\n", &total)
-	var arr = make([]string, total) // 입력을 받을 배열 선언
-	var temp = make([]int, total)   // 점수를 넣을 배열 선언
-	for i := 0; total > i; i++ {
-		fmt.Fscanln(reader, &arr[i])
-		var correct, score int // O의 유무와 점수를 위한 변수 선언
+	input, _ = reader.ReadByte()
 
-		for j := 0; len(arr[i]) > j; j++ {
-			if fmt.Sprintf("%c", arr[i][j]) == "O" {
-				correct++
-				score += correct
-			} else {
-				correct = 0
-			}
-		}
-		temp[i] = score
-	}
-	// fmt.Println(temp)
-	for _, e := range temp {
-		fmt.Println(e)
-	}
+	fmt.Printf("%d\n", input)
 }
