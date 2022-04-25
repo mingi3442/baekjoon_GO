eee40ec57f0b2050bcf84a4b7c504a2c0a621868
mingi3442 <mingi3442@gmail.com>
2022-04-25 23:57:52 +0900

    Euclidean algorithm

--- main.go
+++ main.go
@@ -7,23 +7,23 @@ import (
 )
 
 func main() {
-	var h, m int
-	var hour, min, add int
+	var a, b int
 	reader := bufio.NewReader(os.Stdin)
-	// writer := bufio.NewWriter(os.Stdin)
-	fmt.Fscanf(reader, "%d %d\n%d", &hour, &min, &add)
-	// defer writer.Flush()
-	h = add / 60
-	m = add % 60
-	hour = hour + h
-	min = min + m
-	if min > 59 {
-		min = min - 60
-		hour++
+	fmt.Fscanf(reader, "%d %d", &a, &b) // a,b를 입력 받는다
+	if a < b {
+		a, b = b, a // 나머지 연산을 하기 위해 b가 a보다 클 경우 자리를 바꿔준다
 	}
-	if hour > 23 {
-		hour = hour - 24
+	gcd := 0 // 최대공약수 선언
+	for {
+		r := a % b
+		if r == 0 {
+			//나머지 연산 값이 0일 경우 최대 공약수 값은 0이 되며 반복문은 종료
+			gcd = b
+			break
+		}
+		// 나머지 연산 값이 0이 아닐 경우  a에는 b를, b에는 나머지 연산 값을 선언해서 반복
+		a = b
+		b = r
 	}
-	// fmt.Fprintln(writer, hour, min)
-	fmt.Println(hour, min)
+	fmt.Println(gcd)
 }
